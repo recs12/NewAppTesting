e990d3dcf7b3c9915b032507de62d085ac4ca5f1
recs12 <slimane.rechdi@gmail.com>
2021-06-01 06:57:29 -0400

    first

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿module AppTests
+
+open Expecto
+
+open NewAppBasic.Counter
+
+let avaloniaTests = 
+    testList "NewAppBasic" [
+        test "[conv] string -> list string" {
+            let someResult = splitIntoList "abc"
+            Expect.equal someResult ['a'; 'b'; 'c'] "Result is a list of char"
+        }
+
+        test "[conv] list string -> string with spaces" {
+            let someResult = createWordFromList ['a'; 'b'; 'c']
+            Expect.equal someResult "a b c" "Result is a list of char"
+        }
+
+        test "[length] list -> int" {
+            let someResult = getLengthFromList ['a'; 'b'; 'c']
+            Expect.equal someResult 3 "Number of items in the list."
+        }
+    ]
+
+
+[<EntryPoint>]
+let main argv =
+    runTestsWithArgs defaultConfig argv avaloniaTests
